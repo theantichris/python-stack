b280a13d54f3bd6836d642b53030c4528a751f53
Christopher Lamm <chris@theantichris.com>
2019-12-04 07:26:30 -0500

    added pop method

--- stack.py
+++ stack.py
@@ -11,3 +11,8 @@ class Stack:
         item = Node(value)
         item.set_next_node(self.top_item)
         self.top_item = item
+
+    def pop(self):
+        item_to_remove = self.top_item
+        self.top_item = item_to_remove.get_next_node()
+        return item_to_remove.get_value()
