234850a5b2509d79427734d2b04fa6e7848a0547
Christopher Lamm <chris@theantichris.com>
2019-12-04 07:38:00 -0500

    refactored to is_empty function

--- stack.py
+++ stack.py
@@ -7,7 +7,7 @@ class Stack:
         self.size = 0
 
     def peek(self):
-        if self.size > 0:
+        if not self.is_empty():
             return self.top_item.get_value()
 
     def push(self, value):
@@ -20,7 +20,7 @@ class Stack:
             print("The stack is full, you cannot add anymore Nodes.")
 
     def pop(self):
-        if self.size > 0:
+        if not self.is_empty():
             item_to_remove = self.top_item
             self.top_item = item_to_remove.get_next_node()
             self.size -= 1
@@ -28,3 +28,6 @@ class Stack:
 
     def has_space(self):
         return self.limit > self.size
+
+    def is_empty(self):
+        return self.size == 0
